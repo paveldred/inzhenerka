69bb1411401e6b873eaea7ef45dd6b8eb9940867
Pavel Smolkov <smolcov@bk.ru>
2024-07-28 17:48:59 +0700

    Combined intermediate commits into one

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 # Example program for Git course
 
-txt_arr = ['I', 'love', 'Git', 'so much']
+txt_arr = ['I', 'love', 'Git', 'so much', 'at home']
 txt = ' '.join(txt_arr) + '!'
 print(txt)
 print('Done')
